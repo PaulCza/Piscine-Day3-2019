cd9e11d4cca52681c774e6ec0948ef94e10ef278
Paul Czaplinski <162437852+PaulCza@users.noreply.github.com>
2024-09-09 10:10:34 +0200

    Add files via upload

---
+++ tests/tests_my_put_nbr.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** tests_my_put_nbr.c
+** File description:
+** sfiffds
+*/
+
+int main()
+{
+    my_isneg(0);
+    my_isneg(21);
+    my_isneg(-21);
+    return (0)
+        }
